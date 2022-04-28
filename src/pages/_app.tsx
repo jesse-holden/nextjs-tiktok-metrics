26d48c793e9b740961096f07fc7c8e254c2ad817
Jesse Holden <jesse.holden@cyberfiend.com>
2022-04-28 17:51:04 -0400

    style: add framer-motion for page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
+import { motion } from 'framer-motion';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <motion.div
+      key={router.route}
+      initial='initial'
+      animate='animate'
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
 
 export default MyApp;

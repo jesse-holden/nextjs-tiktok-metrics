4be36e5af4f9ed5491478afcf85991f47d70c6c9
Jesse Holden <jesse.holden@cyberfiend.com>
2022-05-01 17:06:42 -0400

    chore: add dependencies and config for Vercel production environment

--- src/lib/tiktok-helpers.ts
+++ src/lib/tiktok-helpers.ts
@@ -1,7 +1,28 @@
-import puppeteer from 'puppeteer';
+import Chrome from 'chrome-aws-lambda';
+import puppeteerLib from 'puppeteer';
+
+let chrome = {
+  args: [],
+} as unknown as typeof Chrome;
+let puppeteer: typeof puppeteerLib;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+} else {
+  // running locally.
+  puppeteer = require('puppeteer');
+}
 
 export async function fetchTikTokURL(url: string): Promise<string> {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
   const page = await browser.newPage();
   await page.setExtraHTTPHeaders({
     'user-agent':
